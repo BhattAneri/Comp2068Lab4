fada9b533b23d1bb2408f4132aad9d985270db0f
BhattAneri <200430090@student.georgianc.on.ca>
2020-06-16 17:29:36 -0400

    Deploying for last time

--- controllers/ProductsController.js
+++ controllers/ProductsController.js
@@ -2,27 +2,16 @@ const viewPath = ('products');
 const Product = require('../models/product');
 
 exports.show = async (req, res) => {
-  try {
-    const product = await product.findById(req.params.id);
-    res.render(`${viewPath}/show`, {
-      pageTitle: product.title,
-      product: product
+    Product.findById(req.params.id)
+    .then(product => {
+      res.render(`${viewPath}/show`, {
+        pageTitle: product.name,
+        product: product
+      });
+    })
+    .catch(err => {
+      console.error(`ERROR: ${err}`);
     });
-  } catch (error) {
-    req.flash('danger', `There was an error displaying this product: ${error}`);
-    res.redirect('/');
-  }
-};
-    // Product.findById(req.params.id)
-    // .then(product => {
-    //   res.render(`${viewPath}/show`, {
-    //     pageTitle: product.name,
-    //     product: product
-    //   });
-    // })
-    // .catch(err => {
-    //   console.error(`ERROR: ${err}`);
-    // });
   };
 
 exports.new = (req, res) => {
@@ -32,21 +21,12 @@ exports.new = (req, res) => {
 };
 
 exports.create = async (req, res) => {
-  try {
-    const product = await product.create(req.body);
-    req.flash('success', 'product created successfully');
-    res.redirect(`/products/${product.id}`);
-  } catch (error) {
-    req.flash('danger', `There was an error creating this product: ${error}`);
-    req.session.formData = req.body;
-    res.redirect('/products/new');
-  }
 
-  // Product.create(req.body.product)
-  // .then(() => {
-  //   res.redirect(`${viewPath}/show`);
-  // })
-  // .catch(err => {
-  //   console.error(`ERROR: ${err}`);
-  // });
+  Product.create(req.body.product)
+  .then(() => {
+    res.redirect(`${viewPath}/show`);
+  })
+  .catch(err => {
+    console.error(`ERROR: ${err}`);
+  });
 };
